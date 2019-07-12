f6e64378b5be8f47c1238eaf7adfa8cebd2f5e69
Roman Kananovich <roman.kananovich@gmail.com>
2019-07-12 15:31:20 +0300

    empty project

---
+++ GreenTeamProject/GreenTeamProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GreenTeamProject
+//
+//  Created by Roma on 7/12/19.
+//  Copyright © 2019 GreenTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GreenTeamProject/GreenTeamProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GreenTeamProject
+//
+//  Created by Roma on 7/12/19.
+//  Copyright © 2019 GreenTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
